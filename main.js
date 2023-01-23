44058e1806fa979793a6305442fe22142a1f29d2
eri-imai <imai.eri.0415@gmail.com>
2023-01-23 10:43:37 +0900

    ハンバーガーメニュー

---
+++ main.js
@@ -0,0 +1,7 @@
+
+
+
+document.querySelector('.hamburger').addEventListener('click', function(){
+  this.classList.toggle('active');
+  document.querySelector('.slide-menu').classList.toggle('active');
+})
