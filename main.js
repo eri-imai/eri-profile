1e9c5c3c3c579b47333ff00a14d266bf6262ecc1
eri-imai <imai.eri.0415@gmail.com>
2023-02-03 12:48:45 +0900

    JS追加

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
+// ハンバーガーメニュー
 
-
-
-document.querySelector('.hamburger').addEventListener('click', function(){
+document.querySelector('.hamburger').addEventListener('click', function () {
   this.classList.toggle('active');
   document.querySelector('.slide-menu').classList.toggle('active');
 })
+
+// フェードイン
+
+function fadeinEvent() {
+  var fadeinClass= Array.prototype.slice.call(document.getElementsByClassName("fadein"));
+
+  fadeinClass.forEach(function( element ) {
+      var boundingClientRect = element.getBoundingClientRect();
+      var scroll = document.documentElement.scrollTop || document.body.scrollTop;
+      var windowHeight = window.innerHeight;
+
+      if (scroll > scroll + boundingClientRect.top - windowHeight + 200){
+          element.style.opacity = "1";
+          element.style.transform = "translateY(0)";
+      }
+  });
+}
+
+window.onload = function(){
+  fadeinEvent();
+  window.addEventListener('scroll', fadeinEvent, false);
+}
+
+
